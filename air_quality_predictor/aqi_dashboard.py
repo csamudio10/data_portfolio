9a4f0112d93ce14604bf194b0c50705e1e52f598
csamudio10 <151407734+csamudio10@users.noreply.github.com>
2024-02-06 14:30:12 -0500

    Update aqi_dashboard.py

--- air_quality_predictor/aqi_dashboard.py
+++ air_quality_predictor/aqi_dashboard.py
@@ -3,3 +3,23 @@ import pandas as pd
 import numpy as np
 
 st.title('Air Quality in Usaquen')
+
+data_url = 'https://github.com/csamudio10/portfolio/blob/main/air_quality_predictor/aqi_usaquen.csv'
+date_column = 'time'
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(data_url, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[date_column] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
