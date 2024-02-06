730657c655f1dea379f805b026f482e55f05e904
csamudio10 <151407734+csamudio10@users.noreply.github.com>
2024-02-06 16:23:26 -0500

    Update aqi_dashboard.py

--- air_quality_predictor/aqi_dashboard.py
+++ air_quality_predictor/aqi_dashboard.py
@@ -9,6 +9,7 @@ date_column = 'time'
 
 def load_data():
     data = pd.read_csv(data_url)
+    data['time'] = pd.datetime(data['time'],format='%Y-%m-%d %H:%M:%S)
     return data
 
 data_load_state = st.text('Loading data...')
@@ -18,4 +19,9 @@ data_load_state.text("Done! (using st.cache_data)")
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
+
+col1, col2, col3 = st.columns([1,3,1])
+
+with col2:
+    st.line_chart(data[['time','aqi']])
 
